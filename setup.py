934b381686b9311da4c8a15baa0b0ecc1a89c0fd
kjradem <koenrademaker@outlook.com>
2018-12-03 23:03:53 +0100

    added first draft for command line interaction

--- setup.py
+++ setup.py
@@ -6,19 +6,22 @@ def read_file(file_path):
         return f.read()
 
 
-setup(
-    name='ConFInR',
-    version='',
-    description='ConFInR is a Python tool for functional annotation of metagenomics sequences.',
-    long_description=read_file('README.md'),
-    author='Koen Rademaker',
-    author_email='koenrademaker@outlook.com',
-    url='https://github.com/kjradem/ConFInR',
-    py_modules=['confinr'],
-    license=read_file('LICENSE'),
-    entry_points={
-        'console_scripts': [
-            'aloha = helloworld:hello'
-        ]
-    }
-)
+# TODO: Fix setup to enable command line executability.
+# setup(
+#     name='ConFInR',
+#     version='',
+#     description='ConFInR is a Python tool for functional annotation of metagenomics sequences.',
+#     long_description=read_file('README.md'),
+#     author='Koen Rademaker',
+#     author_email='koenrademaker@outlook.com',
+#     url='https://github.com/kjradem/ConFInR',
+#     py_modules=['confinr'],
+#     license=read_file('LICENSE'),
+#     entry_points={
+#         'console_scripts': [
+#             'confinr-init = confinr:initialize',
+#             'confinr-preprocess = confinr:preprocess',
+#             'confinr-run = confinr:run'
+#         ]
+#     }
+# )
