53c68c0dfb971f7986ec9c21957d4b6322c541a6
kjradem <koenrademaker@outlook.com>
2018-12-04 12:24:07 +0100

    updated console scripts

--- setup.py
+++ setup.py
@@ -6,22 +6,25 @@ def read_file(file_path):
         return f.read()
 
 
-# TODO: Fix setup to enable command line executability.
-# setup(
-#     name='ConFInR',
-#     version='',
-#     description='ConFInR is a Python tool for functional annotation of metagenomics sequences.',
-#     long_description=read_file('README.md'),
-#     author='Koen Rademaker',
-#     author_email='koenrademaker@outlook.com',
-#     url='https://github.com/kjradem/ConFInR',
-#     py_modules=['confinr'],
-#     license=read_file('LICENSE'),
-#     entry_points={
-#         'console_scripts': [
-#             'confinr-init = confinr:initialize',
-#             'confinr-preprocess = confinr:preprocess',
-#             'confinr-run = confinr:run'
-#         ]
-#     }
-# )
+def read_lines(file_path):
+    with open(file_path) as f:
+        return f.read().splitlines()
+
+
+# TODO: ADD LICENSE IMPORT
+setup(
+    name='ConFInR',
+    version='0.1',
+    description='ConFInR is a Python tool for functional annotation of metagenomics sequences.',
+    long_description=read_file('README.md'),
+    author='Koen Rademaker',
+    author_email='koenrademaker@outlook.com',
+    url='https://github.com/kjradem/ConFInR',
+    py_modules=['confinr'],
+    install_requires=read_lines('requirements.txt'),
+    entry_points='''
+            [console_scripts]
+            confinr-preprocess=confinr:preprocessing
+            confinr-run=confinr:run
+        '''
+)
